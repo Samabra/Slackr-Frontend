ac00aaa478dbab5e218e0f7c049b1be134e3452e
Sameen Chowdhury <sameenchowdhury@Sameens-MacBook-Air.local>
2025-10-23 22:11:47 +1100

    Created a html only on DOM manipulation now for login, just building the basics

--- frontend/src/screens/login.js
+++ frontend/src/screens/login.js
@@ -8,17 +8,32 @@ export function renderLogin({ mount, go }) {
 
     const emailLabel = document.createElement('label');
     emailLabel.innerText = 'Email';
+
     const emailInput = document.createElement('input');
     emailInput.type = 'text';
-    emailInput.id = 'login-user'
-    
-    mount.innerHTML = `
-    <h2>Login</h2>
-    Email: <input type="text" id="login-user" />
-    Password: <input type="password" id="login-password" />
-    <button id="login-button">Login</button>
-    `
-    mount.querySelector('#login-button').addEventListener('click', () => {
+    emailInput.id = 'login-user';
+
+    const passwordLabel = document.createElement('label');
+    passwordLabel.innerText = 'Password';
+
+    const passwordInput = document.createElement('input');
+    passwordInput.type = 'password';
+    passwordInput.id = 'login-password';
+
+    const loginButton = document.createElement('button');
+    loginButton.innerText = 'Login';
+    loginButton.id = 'login-button';
+
+    mount.appendChild(loginTitle);
+    mount.appendChild(emailLabel);
+    mount.appendChild(emailInput);
+    mount.appendChild(document.createElement('br'));
+    mount.appendChild(passwordLabel);
+    mount.appendChild(passwordInput);
+    mount.appendChild(document.createElement('br'));
+    mount.appendChild(loginButton);
+
+    loginButton.addEventListener('click', () => {
         const email = mount.querySelector('#login-user').value;
         const password = mount.querySelector('#login-password').value;
     });
