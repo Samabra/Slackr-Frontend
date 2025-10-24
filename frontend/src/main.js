bd462b9ea2c555ff520302220d177a23398afcdf
Sameen Chowdhury <sameenchowdhury@Sameens-MacBook-Air.local>
2025-10-24 15:32:26 +1100

    Refined the main.js in proper file

---
+++ frontend/src/main.js
@@ -0,0 +1,37 @@
+import { BACKEND_PORT } from './config.js';
+
+// A helper you may want to use when uploading new images to the server.
+
+import { fileToDataUrl } from './helpers.js';
+import { renderLogin } from './screens/login.js'
+
+  const mount = document.querySelector('[role="main"]');
+
+  
+
+const routes = {
+    login: renderLogin,
+};
+
+  
+  
+
+function go(name) {
+    while (mount.firstChild) {
+        mount.removeChild(mount.firstChild);
+    }   
+
+    const token = localStorage.getItem('token');
+    const protectedScreen = new Set(['home']);
+    if (!token && protectedScreen.has(name)) {
+        name = 'login';
+    }
+    const screen = routes[name];
+
+    if (screen) {
+        screen({ mount, go });
+    }
+
+}
+
+go('login');
