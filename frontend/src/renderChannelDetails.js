7e7c40efbf7b0db1cda64ab9b1638b19bd805d49
Sameen Chowdhury <sameenchowdhury@Sameens-MacBook-Air.local>
2025-11-02 11:57:05 +1100

    Created a new file called renderChannelDetails which will render the channel details and be able to update details as well

---
+++ frontend/src/renderChannelDetails.js
@@ -0,0 +1,42 @@
+import { API_BASE } from "./config.js";
+import { showError } from "./errorPopup.js";
+import { renderChannels } from "./helpers.js";
+
+function getChannel(channelId) {
+    return fetch(`${API_BASE}/channel/${channelId}`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+    })
+        .then(res => res.json.then(data => ({ ok: res.ok, data})))
+        .then(({ ok, data }) => {
+            if (!ok) {
+                throw new Error(data.error || 'Failed to load channel details');
+            }
+            return data;
+        })
+}
+
+
+function updateChannel(channelId, name, description) {
+    return fetch(`${API_BASE}/channel/${channelId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify ({
+            name: name,
+            description: description,
+        })
+    })
+        .then(res => res.json.then(data => ({ ok: res.ok, data})))
+        .then(({ ok, data }) => {
+            if (!ok) {
+                throw new Error(data.error || 'Failed to load channel details');
+            }
+            return data;
+        })
+}
