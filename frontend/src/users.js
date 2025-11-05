9dade92eb7d382e1b85cb9a4f28351cd0171403f
Sameen Chowdhury <sameenchowdhury@Sameens-MacBook-Air.local>
2025-11-05 11:59:09 +1100

    Adding all the API endpoints that are required for inviting users, getting user profile and etc

--- frontend/src/users.js
+++ frontend/src/users.js
@@ -1,5 +1,6 @@
 import { API_BASE } from "./config.js";
 import { showError } from "./errorPopup.js";
+import { getChannel } from "./helpers.js";
 
 function getAllUsers() {
     return fetch(`${API_BASE}/user`, {
@@ -18,3 +19,66 @@ function getAllUsers() {
         });
 }
 
+
+function userInvite(channelId, userID) {
+    return fetch(`${API_BASE}/channel/${channelId}/invite`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+            userId: userId,
+        })
+    })
+        .then(res => res.json().then(data => ({ ok: res.ok, data})))
+        .then(({ ok, data }) => {
+            if (!ok) {
+                throw new Error(data.error || 'Failed to load channel details');
+            }
+            return data;
+        });
+}
+
+
+function getUserProfile(userId) {
+    return fetch(`${API_BASE}/channel/${channelId}/invite`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+    })
+        .then(res => res.json().then(data => ({ ok: res.ok, data})))
+        .then(({ ok, data }) => {
+            if (!ok) {
+                throw new Error(data.error || 'Failed to load channel details');
+            }
+            return data;
+        });
+}
+
+
+function updateUserProfile(email, password, name, bio, image) {
+    return fetch(`${API_BASE}/channel/${channelId}/invite`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+            name: name,
+            bio: bio,
+            image: image,
+        })
+    })
+        .then(res => res.json().then(data => ({ ok: res.ok, data})))
+        .then(({ ok, data }) => {
+            if (!ok) {
+                throw new Error(data.error || 'Failed to load channel details');
+            }
+            return data;
+        });
+}
